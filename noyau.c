5c992f0434497d7a9db8d97bb4177a5db823ea16
wolusien <djebali.wissam@gmail.com>
2015-11-28 17:43:38 +0100

    Blabla

--- noyau.c
+++ noyau.c
@@ -5,7 +5,7 @@ int main (int argc, char * argv[]){
   disk.name="fic.txt";
   block b;
   read_physical_block(disk,b,00000000000000000000000000000001);
-  printf("%s\n", b.b);
+  printf("Lucas %s\n", b.b);
   exit(0);
 }
 
