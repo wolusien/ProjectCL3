72317e983696acf2029f1b91f50ff1a50eed067a
llabad39 <lucaslabadens@MacBook-Pro-de-Lucas-2.local>
2015-11-28 15:26:28 +0100

    modif noyau

--- noyau.c
+++ noyau.c
@@ -14,7 +14,7 @@ int main (int argc, char * argv[]){
   disk.name="disk.tfs";
   block b;
   read_physical_block(disk,b,00000000000000000000000000000001);
-  printf("%s\n",b.b);
+  printf("Voici le message%s il estfini\n",b.b);
   exit(0);
 }
 
