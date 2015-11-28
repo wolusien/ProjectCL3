d13d48d6321b18c17865431be8fd92a28620545d
llabad39 <lucaslabadens@MacBook-Pro-de-Lucas-2.local>
2015-11-28 13:46:31 +0100

    interne.c

---
+++ interne.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include "interne.h"
+error read_physical_block(disk_id id,block b,uint32_t num){
+  FILE *f = fopen(id.name,r);
+  fseek(f,num*1024,SEEK_SET);
+  fgets(&(b.b),1024,f);
+}
