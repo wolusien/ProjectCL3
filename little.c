f07cec6bb506f24ae28bf96ca6439e87d9676e02
wolusien <djebali.wissam@gmail.com>
2015-12-14 14:45:32 +0100

    Mise à jour de l'arborescence avec ajout de little.c et little.h

---
+++ little.c
@@ -0,0 +1,18 @@
+#include <little.h>
+
+uint32_t int_to_little(int x){
+    int v=x;
+    uint32_t r;
+    unsigned char *t =(unsigned char*)&r;
+    t[0]=x%256;
+    t[1]=(x/256)%256;
+    t[2]=((x/256)/256)%256;
+    t[3]=(((x/256)/256)/256)%256;
+    return r;
+}
+
+int little_to_int(uint32_t x){
+    unsigned char *buf=(unsigned char*)(&x);
+    int b=(buf[0] + 256*buf[1] +(256*256)*buf[2] +(256*256*256)*buf[3]);
+    return b;
+}
---
+++ little.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+uint32_t int_to_little(int x);
+
+int little_to_int(uint32_t x);
