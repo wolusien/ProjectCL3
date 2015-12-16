b1e97b931a0afab85e485dc6f1d0984e41cb95bc
wolusien <djebali.wissam@gmail.com>
2015-12-16 15:31:02 +0100

    MOdif de  main.c

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include "interne.h"
 
 int main (int argc, char * argv[]){
-    printf("Je suis au début du codedu main\n");
+    printf("Je suis au début du code du main\n");
     disk_id *id;
-    char *path = "disk.tfs"; 
+    char *path = "disk.tfs";
+    printf("Je vais exécuter startdisk\n");
     error start=start_disk(path,id);
     block b;
     uint32_t num = int_to_little(0);
@@ -16,7 +17,14 @@ int main (int argc, char * argv[]){
     b.id=0;
     printf("Je vais exécuter write_physical\n");
     write_physical_block((*id),b,num);
-    //error stop=stop_disk((*id));
+    printf("Je vais exécuter read_physical\n");
+    read_physical_block((*id),b,num);
+    for(i=0; i<4; i++){
+        o[i]=b.b[i];
+    }
+    printf("Voici votre nombre %d\n",int_to_little(n));
+    printf("Je vais exécuter stopdisk\n");
+    error stop=stop_disk((*id));
     exit(0);
 }
 
