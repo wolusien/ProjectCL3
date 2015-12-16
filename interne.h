4166a096a0f5a7049a7c57db372a52d6402a9378
wolusien <djebali.wissam@gmail.com>
2015-12-16 14:54:34 +0100

    Modification de startdisk

--- interne.h
+++ interne.h
@@ -14,12 +14,13 @@
 #define BUFSIZE 256;
 #define MAX_DISQUE 100
 
-FILE *disque_ouvert[MAX_DISQUE];
-
 typedef struct{
   int id;
   char *name;
+  FILE* fd;
 }disk_id;
+
+disk_id* disque_ouvert[MAX_DISQUE];
 
 typedef struct{
   char b[1024];
@@ -28,3 +29,5 @@ typedef struct{
 error read_physical_block(disk_id id,block b,uint32_t num);
 error write_physical_block(disk_id id,block b,uint32_t num);
 error string_to_uint (char *s, uint32_t u);
+error start_disk(char *name,disk_id *id);
+error stop_disk(disk_id id);
