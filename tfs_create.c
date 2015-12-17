add04ea8e21bebc7874622ef95b1e1b381aa87ac
llabad39 <lucaslabadens@MacBook-Pro-de-Lucas-2.local>
2015-12-17 21:07:18 +0100

    e

--- tfs_create.c
+++ tfs_create.c
@@ -1,4 +1,4 @@
-/*#include "interne.h"
+#include "interne.h"
 
 int main(int argc,char *argv[]){
   int fichier;
@@ -7,7 +7,6 @@ int main(int argc,char *argv[]){
     int err = open("disk.tfs",O_CREAT | O_EXCL);
     if(err !=-1){
       fichier =err;
-      
     }
     else{
       printf("le nom par défaut existe déjà veuillez preciser un nouveau nom de fichier");
@@ -15,4 +14,4 @@ int main(int argc,char *argv[]){
     }
   }
 }
-*/
+
