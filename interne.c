fe33d9b5fbff01e13a3c193d3459b13a3de3a9ac
llabad39 <lucaslabadens@macbook-pro-de-lucas-2.home>
2015-12-22 11:25:54 +0100

    e

--- interne.c
+++ interne.c
@@ -6,12 +6,7 @@ error read_physical_block(disk_id id,block b,uint32_t num){
         if(disque_ouvert[id.id]!=NULL){
             int f = disque_ouvert[id.id]->fd;
             if (f!=-1){
-                //Recupère le nb de block dans le disk
-                uint32_t nb_block;
-                unsigned char* u = (unsigned char*)(&nb_block);
-                lseek(f,0,SEEK_SET);
-                read(f,u,4);
-                if(num<=nb_block){
+	      if(num<=id.nbBlock){
                     lseek(f,num*1024,SEEK_SET);
                     read(f,b.buff,1024);
                     e.errnb=0;
@@ -36,12 +31,7 @@ error write_physical_block(disk_id id,block b,uint32_t num){
         if(disque_ouvert[id.id]!=NULL){
             int f = id.fd;
             if (f!=-1){
-                //Recupère le nb de block dans le disk
-                uint32_t nb_block;
-                unsigned char* o = (unsigned char*)(&nb_block);
-                lseek(f,0,SEEK_SET);
-                read(f,o,4);
-                if(num<=nb_block){
+	      if(num<=id.nbBlock){
                     lseek(f,num*1024,SEEK_SET);
                     write(f,b.buff,1024);
                     e.errnb=0;
--- tfs_create.c
+++ tfs_create.c
@@ -1,27 +1,26 @@
 #include "interne.h"
 
 int main(int argc,char *argv[]){
-    int fichier;
-    int size;
-    char *name;
-    printf("%d",argc);
+  int fichier;
+  int size;
+  char *name;
   switch(argc){
   case 1 :
-      name = "disk.tfs";
+    name = "disk.tfs";
       size = DEFAULT_SIZE_TFS;
       break;
   case 2 :
-      name = argv[1];
-      size = DEFAULT_SIZE_TFS;
-      break;
+    name = argv[1];
+    size = DEFAULT_SIZE_TFS;
+    break;
   case 4 :
     if(strcmp(argv[1],"-s")==0){
       name = argv[3];
       size = atoi(argv[2]);
     }
     else{
-        printf("option %s inconnue changez pour -s",argv[1]);
-        exit(-1);
+      printf("option %s inconnue changez pour -s",argv[1]);
+      exit(-1);
     }
     break;
   default:
@@ -29,17 +28,17 @@ int main(int argc,char *argv[]){
     exit(-1);
     break;
   }
-  fichier = open(name,O_CREAT | O_EXCL | O_RDWR | S_IRUSR | S_IWUSR);
+  fichier = open(name,O_CREAT | O_EXCL | O_RDWR ,S_IREAD);
   if(fichier != -1){
     block first;
     uint32_t u = int_to_little(size);
-    unsigned char *tab =(unsigned char *)&u;
+    unsigned char *tab = (unsigned char *)&u;
     int i;
     for(i=0;i<4;i++){
       first.buff[i]=tab[i];
     }
     for(i=4;i<1024;i++){
-      first.buff[i]='0';
+      first.buff[i]='f';
     }
     uint32_t position = int_to_little(0);
     disk_id disk;
@@ -53,14 +52,15 @@ int main(int argc,char *argv[]){
     }
     for(i=1;i<size;i++){
       position=int_to_little(i);
+      printf("%d\n",position);
       write_physical_block(disk,rest,position);
     }
     disque_ouvert[0]=NULL;
     close(fichier);
-
+    
   }
   else{
-      printf("le nom choisi existe déjà veuillez en entrez un nouveau");
+    printf("le nom choisi existe déjà veuillez en entrez un nouveau");
   }
 }
 
