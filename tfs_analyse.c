49b74f8c1ea2e4e387b9ee8bffe98af0baead466
wolusien <djebali.wissam@gmail.com>
2015-12-23 14:24:44 +0100

    tfs_analyse réglage de l'affichage

--- tfs_analyse.c
+++ tfs_analyse.c
@@ -8,20 +8,23 @@ int main(int argc, char* argv[]){
     disk_id* disk = malloc(sizeof(disk_id));
     e = start_disk(argv[argc-1],disk);
     if(e.errnb!=-1){
-      fprintf(stdin,"Disk Configuration : %s\n",argv[argc-1]);
+      fprintf(stdout,"\t\t\t\t\tDisk Configuration : %s\n",argv[argc-1]);
       if((*disk).nbPart>0){
-        fprintf(stdin,"\t\t\tPartition number : %d\nList of partitions : \n",(*disk).nbPart);
+        fprintf(stdout,"\t\t\tPartition number : %d\nList of partitions : \n",(*disk).nbPart);
         for(i=0; i<(*disk).nbPart;i++){
           used_space += (*disk).taillePart[i];
-          fprintf(stdin,"Partition %d : \t%dB\n",i,(*disk).taillePart[i]);
+          fprintf(stdout,"Partition %d : \t%dB\n",i,(*disk).taillePart[i]);
         }
       }
       int free_space = (*disk).nbBlock - used_space;
-      fprintf(stdin,"\nDisk : %s\n\t\tBlocks number : %d\tUsed blocks : %d\tFree blocks : %d\n",argv[argc-1],(*disk).nbBlock,used_space,free_space);
-      e = stop_disk(*disk);
+      fprintf(stdout,"\nDisk : %s\n\t\tBlocks number : %d\tUsed blocks : %d\t\tFree blocks : %d\n",argv[argc-1],(*disk).nbBlock,used_space,free_space);
+      stop_disk(*disk);
+      exit(0);
     }else{
       exit(-1);
     }
+  }else{
+    fprintf(stderr,"tfs_analyse : Wrong arguments number\n");
+    exit(-1);
   }
-  exit(0);
 }
