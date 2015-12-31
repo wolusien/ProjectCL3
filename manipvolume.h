834ef05ff0b0d93e7894f6d474167e1817917c92
wolusien <djebali.wissam@gmail.com>
2015-12-31 02:46:38 +0100

    Changement de la signature de add_file_block

--- manipvolume.h
+++ manipvolume.h
@@ -17,4 +17,4 @@ error free_block(disk_id *id, int numblock, int volume);
 error add_free_file(disk_id id,int volume,int file);
 error remove_free_file(disk_id id,int volume,int file);
 iter  decomposition(char *path);
-error add_file_block(disk_id disk,int id_part,int id_f, int id_block);
+error add_file_block(disk_id* disk,int id_part,int id_f, int id_block);
