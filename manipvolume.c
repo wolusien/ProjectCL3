544e4ccbb412089581cc166a8738509688ab8184
llabad39 <lucaslabadens@hotmail.fr>
2016-01-08 15:59:23 +0100

    reglages conflit

--- manipvolume.c
+++ manipvolume.c
@@ -318,36 +318,6 @@ iter decomposition(char *path) {
     printf("fin\n");
     return current;
 }
-void free_iter(iter i){
-    if(i != NULL){
-        iter next = i->next;
-        //iter prec = i->prec;
-        i->name=NULL;
-        free(i->name);
-        i=NULL;
-        free(i);
-        if(next != NULL);
-            free_iter(next);
-    }
-}
-void go_end(iter it){
-    iter cur =it;
-    if(cur !=NULL){
-        while(cur ->next!= NULL){
-            //printf("%s\n",cur->name);
-            cur=cur->next;
-        }
-    }
-    printf("%s\n",cur->name);
-}
-
-void go_start(iter it){
-    if(it != NULL){
-        while(it->prec != NULL){
-           it=it->prec;
-        }
-   }
-}
 
 error readname_rep(block b, char *a, int loc) { //loc est l'endroit du bloc ou on veut lire le nom que l'on écrit dans a.
     int i;
